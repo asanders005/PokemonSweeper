91402ca75d0208fa18a799a836e71ef19277a8e4
Aiden Sanders <aidensandersbiz@gmail.com>
2026-03-07 16:10:58 -0700

    Update post-win window to allow for saving pokemon

--- Game/Messages/Score.xaml.cs
+++ Game/Messages/Score.xaml.cs
@@ -1,4 +1,5 @@
-﻿using PokemonSweeper.Game.PokemonModels;
+﻿using PokemonSweeper.Data;
+using PokemonSweeper.Game.PokemonModels;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
@@ -10,16 +11,20 @@ namespace PokemonSweeper.Game.Messages
     /// </summary>
     public partial class Score : Window
     {
-        public Score()
+        private int _maxSelectablePokemon = 2;
+        private DAL _dal;
+
+        public Score(DAL dal)
         {
+            _dal = dal;
             InitializeComponent();
         }
 
-        public static void ShowScore(GameWindow sender, PokemonSweeper.Field Field)
+        public static void ShowScore(GameWindow sender, PokemonSweeper.Field Field, DAL dal)
         {
             Field.Timer.Stop();
             var PokeList = new List<PlayerPokemon>();
-            var Winner = new Score();
+            var Winner = new Score(dal);
 
             foreach (var square in Field.Squares.Where(s => s.Pokemon != null))
             {
@@ -28,14 +33,40 @@ namespace PokemonSweeper.Game.Messages
             }
             var newScore = sender.Game.CalculateNewScore(Field.Timer, Field.NrOfClicks, PokeList);
             Winner.score.Text = "Good job! You caught all the Pokemon!! Your score is " + newScore;
+
+            Winner._maxSelectablePokemon = sender.Game.Level switch
+            {
+                1 => 2,
+                2 => 3,
+                3 => 4,
+                _ => Winner._maxSelectablePokemon
+            };
+            Winner.SelectionInfo.Text = $"Select up to {Winner._maxSelectablePokemon} Pokemon to save to your PC.";
+
             Winner.Owner = sender;
             Winner.ShowDialog();
         }
 
+        private void ListBoxPokemon_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (ListBoxPokemon.SelectedItems.Count > _maxSelectablePokemon)
+            {
+                foreach (var item in e.AddedItems)
+                {
+                    ListBoxPokemon.SelectedItems.Remove(item);
+                }
+
+                MessageBox.Show($"You can only select {_maxSelectablePokemon} Pokemon to save!", "Selection limit", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            SelectionInfo.Text = $"Selected {ListBoxPokemon.SelectedItems.Count} / {_maxSelectablePokemon} Pokemon.";
+        }
+
         private async void Next_Click(object sender, RoutedEventArgs e)
         {
-            var OwnerWindow = ((GameWindow) Owner);
-            await OwnerWindow.Game.NewField(OwnerWindow);
+            var selectedPokemon = ListBoxPokemon.SelectedItems.Cast<PlayerPokemon>().ToList();
+
+            await _dal.SavePlayerPokemonAsync(selectedPokemon);
             Close();
         }
     }
