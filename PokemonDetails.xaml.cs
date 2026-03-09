069847f3f57c3f2f3b2aac72a981585c49b0110f
Zultrafury <zultrafury@gmail.com>
2026-03-09 15:03:17 -0600

    Squashed commit of the following:
    commit 3a4178464475566a0d9420d15f380be64a5ed153
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Sun Mar 8 23:00:11 2026 -0600
    
        PokemonPC screen added
    
    commit 84d4f36e93c183f7b98fee668b86a5da78119625
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Sun Mar 8 17:26:41 2026 -0600
    
        PokemonDetails page added
    
    commit f2e30a5c908a9338b404b7c25addd9875b6cb03d
    Merge: 4b00cc6 4f40081
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Sun Mar 8 16:16:04 2026 -0600
    
        Merge branch 'master' into zultrafury
    
    commit 4b00cc6067d389d66df033a88c12bd26b8a0b56f
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Thu Mar 5 13:47:39 2026 -0700
    
        Working on Pokemon Details screen

---
+++ PokemonDetails.xaml.cs
@@ -0,0 +1,49 @@
+#nullable enable
+using System.Windows;
+using System.Windows.Media.Imaging;
+using PokemonSweeper.Data;
+using PokemonSweeper.Game;
+using PokemonSweeper.Game.PokemonModels;
+
+namespace PokemonSweeper;
+
+public partial class PokemonDetails : Window
+{
+    public PlayerPokemon CurrentPlayerPokemon { get; set; }
+    public DAL Dal { get; set; }
+    public PokemonDetails(DAL dal)
+    {
+        InitializeComponent();
+        Dal = dal;
+    }
+
+    private async void SetPokemon()
+    {
+        CurrentPlayerPokemon ??= await PlayerPokemon.CreateWithRandomStats(Dal);
+        var p = CurrentPlayerPokemon.Pokemon;
+        
+        PokemonName.Content = char.ToUpper(p.Name[0]) + p.Name[1..];
+        
+        PokemonImage.Source = new BitmapImage(new System.Uri(p.DefaultSprite));
+        
+        PokemonType.Content = p.PrimaryType+" Type";
+        if (p.SecondaryType != null) PokemonType.Content += ", "+p.SecondaryType+" Type";
+
+        PokemonHP.Content = "HP: " + p.BaseStats[PokemonStatsType.HP];
+        PokemonAttack.Content = "Attack: " + p.BaseStats[PokemonStatsType.Attack];
+        PokemonDefense.Content = "Defense: " + p.BaseStats[PokemonStatsType.Defense];
+        PokemonSpAttack.Content = "Sp. Attack: " + p.BaseStats[PokemonStatsType.SpecialAttack];
+        PokemonSpDefense.Content = "Sp. Defense: " + p.BaseStats[PokemonStatsType.SpecialDefense];
+        PokemonSpeed.Content = "Speed: " + p.BaseStats[PokemonStatsType.Speed];
+    }
+
+    private void BackClick(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+    
+    private void WindowLoaded(object sender, RoutedEventArgs e)
+    {
+        SetPokemon();
+    }
+}
---
+++ PokemonPC.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using PokemonSweeper.Data;
+using PokemonSweeper.Game.PokemonModels;
+
+namespace PokemonSweeper;
+
+public partial class PokemonPC : Window
+{
+    public DAL Dal { get; set; }
+    private List<PlayerPokemon> PCPokemon { get; set; } = new();
+    
+    public PokemonPC(DAL dal)
+    {
+        InitializeComponent();
+        Dal = dal;
+    }
+
+    private async void WindowLoaded(object sender, RoutedEventArgs e)
+    {
+        
+        int pid = 1;
+        PlayerPokemon p;
+        do
+        {
+            p = await Dal.GetPlayerPokemonAsync(pid);
+            if (p == null) break;
+            PCPokemon.Add(p);
+            pid++;
+        } while (true);
+
+        for (int i = 0; i < 3; i++)
+        {
+            PokemonPCGrid.ColumnDefinitions.Add(new ColumnDefinition());
+        }
+        for (int i = 0; i < int.Max(1,(int)Math.Ceiling(PCPokemon.Count/3f)); i++)
+        {
+            PokemonPCGrid.RowDefinitions.Add(new RowDefinition());
+        }
+
+        int j = 0;
+        foreach (PlayerPokemon pokemon in PCPokemon)
+        {
+            DockPanel grid = new();
+            Button button = new()
+            {
+                Content = grid
+            };
+            Label label = new Label()
+            {
+                Content = pokemon.Pokemon.Name,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                FontSize = 20,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+            Image image = new Image()
+            {
+                Source = new BitmapImage(new Uri(pokemon.Pokemon.DefaultSprite)),
+                HorizontalAlignment = HorizontalAlignment.Left,
+                Width = 50
+            };
+            
+            Grid.SetColumn(button, j%3);
+            Grid.SetRow(button, j/3);
+            
+            grid.Children.Add(image);
+            DockPanel.SetDock(image,Dock.Left);
+            grid.Children.Add(label);
+            DockPanel.SetDock(label,Dock.Right);
+            PokemonPCGrid.Children.Add(button);
+
+            j++;
+        }
+    }
+    
+    private void BackClick(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
