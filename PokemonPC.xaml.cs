6a1d8d3c0b4f0f246f5e67f3067e8e20276bfe94
Zultrafury <zultrafury@gmail.com>
2026-03-11 11:46:21 -0600

    Squashed commit of the following:
    commit 8ca3ff8c6f64a2302511ac9237007cc635bed9f0
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Wed Mar 11 11:45:54 2026 -0600
    
        PC screen team building complete
    
    commit d2334d6c470f756e57f95fb9930f1df2816f9162
    Merge: 7cad53d 39909d4
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Wed Mar 11 11:28:54 2026 -0600
    
        Merge branch 'master' into zultrafury
    
    commit 7cad53db389607863c0b2cc15ecd36c4e0f5996c
    Merge: f4cffc9 f5563a8
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Wed Mar 11 11:19:30 2026 -0600
    
        Merge branch 'master' into zultrafury
    
    commit f4cffc9767ae0a8dff493b58e180a36b93112121
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Wed Mar 11 01:51:45 2026 -0600
    
        Pokemon team allocation
    
    commit 092255e374a2e049327b72056c91461c8dfc01e8
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Tue Mar 10 21:53:41 2026 -0600
    
        Changed image filtering mode on pc + details pages
    
    commit 2c5d475aaad9b333194c8b0d63aeb937df92b009
    Merge: ebcb0e6 5ca70c3
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Tue Mar 10 21:38:55 2026 -0600
    
        Merge branch 'master' into zultrafury
    
    commit ebcb0e68744eec2060e0adffb49ab072d018030b
    Merge: 62eb8ce eb50005
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Tue Mar 10 21:37:24 2026 -0600
    
        Merge branch 'master' into zultrafury
    
    commit 62eb8ce00d9fa94c18cfcf45189c0002a4a10198
    Merge: 03eb7d4 bc69922
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Tue Mar 10 15:09:35 2026 -0600
    
        Merge branch 'master' into zultrafury
    
    commit 03eb7d49e657bf786896000d5e6677b90c6252f7
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Tue Mar 10 15:02:20 2026 -0600
    
        Box items now show pokemon details
    
    commit 2a0c27e27d19bbb3b69eafb88428627dd3c0204e
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Tue Mar 10 14:37:16 2026 -0600
    
        Removed Test_Startup method duplication
    
    commit 140f58abe2e7b61e0470129888709b2e56650fb9
    Merge: c669ce3 73cb46e
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Tue Mar 10 14:35:18 2026 -0600
    
        Merge branch 'master' into zultrafury
    
    commit c669ce310206c51101ab249ca553264a70056c65
    Merge: 3a41784 745af27
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Mon Mar 9 19:01:35 2026 -0600
    
        Merge branch 'master' into zultrafury
    
    commit 3a4178464475566a0d9420d15f380be64a5ed153
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Sun Mar 8 23:00:11 2026 -0600
    
        PokemonPC screen added
    
    commit 84d4f36e93c183f7b98fee668b86a5da78119625
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Sun Mar 8 17:26:41 2026 -0600
    
        PokemonDetails page added
    
    commit f2e30a5c908a9338b404b7c25addd9875b6cb03d
    Merge: 4b00cc6 4f40081
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Sun Mar 8 16:16:04 2026 -0600
    
        Merge branch 'master' into zultrafury
    
    commit 4b00cc6067d389d66df033a88c12bd26b8a0b56f
    Author: Zultrafury <zultrafury@gmail.com>
    Date:   Thu Mar 5 13:47:39 2026 -0700
    
        Working on Pokemon Details screen

--- PokemonPC.xaml.cs
+++ PokemonPC.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using PokemonSweeper.Data;
@@ -25,23 +27,104 @@ public partial class PokemonPC : Window
 
     private async void WindowLoaded(object sender, RoutedEventArgs e)
     {
-
+        _pokemonTeamService.CurrentTeam ??= await Dal.LoadPokemonTeamAsync();
         
-        int pid = 1;
-        PlayerPokemon p;
-        do
-        {
-            p = await Dal.GetPlayerPokemonAsync(pid);
-            if (p == null) break;
-            PCPokemon.Add(p);
-            pid++;
-        } while (true);
+        LoadTeamPokemon();
 
+        LoadPcPokemon();
+    }
+
+    private async void LoadTeamPokemon()
+    {
+        PokemonTeamGrid.Children.Clear();
+        
+        for (int i = 0; i < 6; i++)
+        {
+            var pokemon = _pokemonTeamService.CurrentTeam.Pokemon[i];
+            DockPanel grid = new();
+            Button button = new()
+            {
+                Content = grid
+            };
+            string pokemonname = pokemon != null ? char.ToUpper(pokemon.Pokemon.Name[0]) + pokemon.Pokemon.Name[1..] : "None";
+            ImageSource pokemonimage = pokemon != null ?  new BitmapImage(new Uri(pokemon.Pokemon.DefaultSprite)) : null;
+
+            Label label = new()
+            {
+                Content = pokemonname,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                FontSize = 20,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+            Image image = new()
+            {
+                Source = pokemonimage,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                Width = 50
+            };
+
+            void DetailsOpen()
+            {
+                PokemonDetails details = new PokemonDetails(Dal)
+                {
+                    CurrentPlayerPokemon = pokemon
+                };
+                RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.NearestNeighbor);
+                details.Show();
+            }
+
+            var i1 = i;
+            void AddToBox()
+            {
+                _pokemonTeamService.CurrentTeam.Pokemon[i1] = null;
+                LoadTeamPokemon();
+                LoadPcPokemon();
+            }
+
+            if (pokemon != null) button.Click += (_, _) => DetailsOpen();
+            if (pokemon != null) button.MouseRightButtonDown += (_, _) => AddToBox();
+            
+            Grid.SetColumn(button, i%3);
+            Grid.SetRow(button, i/3);
+            
+            grid.Children.Add(image);
+            DockPanel.SetDock(image,Dock.Left);
+            grid.Children.Add(label);
+            DockPanel.SetDock(label,Dock.Right);
+            PokemonTeamGrid.Children.Add(button);
+        }
+    }
+    private async void LoadPcPokemon()
+    {
+        PokemonPCGrid.ColumnDefinitions.Clear();
+        PokemonPCGrid.RowDefinitions.Clear();
+        PokemonPCGrid.Children.Clear();
+
+        if (PCPokemon.Count == 0)
+        {
+            int pid = 1;
+            do
+            {
+                var p = await Dal.GetPlayerPokemonAsync(pid);
+                if (p == null) break;
+                PCPokemon.Add(p);
+                pid++;
+            } while (true);
+        }
+        
+        int[] blacklist = new int[6];
+        for (int i = 0; i < 6; i++)
+        {
+            var p = _pokemonTeamService.CurrentTeam.Pokemon[i];
+            blacklist[i] = p?.PlayerPokemonId ?? -1;
+        }
+        
         for (int i = 0; i < 3; i++)
         {
             PokemonPCGrid.ColumnDefinitions.Add(new ColumnDefinition());
         }
-        for (int i = 0; i < int.Max(1,(int)Math.Ceiling(PCPokemon.Count/3f)); i++)
+
+        for (int i = 0; i < int.Max(1, (int)Math.Ceiling(PCPokemon.Count / 3f)); i++)
         {
             PokemonPCGrid.RowDefinitions.Add(new RowDefinition());
         }
@@ -49,6 +132,8 @@ public partial class PokemonPC : Window
         int j = 0;
         foreach (PlayerPokemon pokemon in PCPokemon)
         {
+            if (blacklist.Contains(pokemon.PlayerPokemonId)) continue;
+            
             DockPanel grid = new();
             Button button = new()
             {
@@ -56,7 +141,7 @@ public partial class PokemonPC : Window
             };
             Label label = new Label()
             {
-                Content = pokemon.Pokemon.Name,
+                Content = char.ToUpper(pokemon.Pokemon.Name[0]) + pokemon.Pokemon.Name[1..],
                 HorizontalAlignment = HorizontalAlignment.Right,
                 FontSize = 20,
                 VerticalAlignment = VerticalAlignment.Center
@@ -70,12 +155,29 @@ public partial class PokemonPC : Window
 
             void DetailsOpen()
             {
-                PokemonDetails details = new PokemonDetails(Dal);
-                details.CurrentPlayerPokemon = pokemon;
+                PokemonDetails details = new PokemonDetails(Dal)
+                {
+                    CurrentPlayerPokemon = pokemon
+                };
+                RenderOptions.SetBitmapScalingMode(image, BitmapScalingMode.NearestNeighbor);
                 details.Show();
+            }
+            
+            void AddToTeam()
+            {
+                for (int i = 0; i < _pokemonTeamService.CurrentTeam.Pokemon.Length; i++)
+                {
+                    ref var p = ref _pokemonTeamService.CurrentTeam.Pokemon[i];
+                    if (p != null) continue;
+                    p = pokemon;
+                    LoadTeamPokemon();
+                    LoadPcPokemon();
+                    break;
+                }
             }
 
             button.Click += (_, _) => DetailsOpen();
+            button.MouseRightButtonDown += (_, _) => AddToTeam();
             
             Grid.SetColumn(button, j%3);
             Grid.SetRow(button, j/3);
@@ -89,14 +191,18 @@ public partial class PokemonPC : Window
             j++;
         }
     }
-
-    private void LoadPcPokemon()
-    {
-
-    }
     
-    private void BackClick(object sender, RoutedEventArgs e)
+    private async void BackClick(object sender, RoutedEventArgs e)
     {
+        try
+        {
+            await Dal.SavePokemonTeamAsync(_pokemonTeamService.CurrentTeam);
+        }
+        catch (NullReferenceException error)
+        {
+            await Console.Error.WriteLineAsync("Warning: team is incomplete and will not be written to disk!");
+        }
+
         Close();
     }
 }
